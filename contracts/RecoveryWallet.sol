8e7800b2693b229f1298c5bc4fb1b381288b68f2
Or Neeman <or@clabs.co>
2020-10-01 08:56:29 -0600

    transfer working and tested

--- contracts/RecoveryWallet.sol
+++ contracts/RecoveryWallet.sol
@@ -13,12 +13,16 @@ contract RecoveryWallet {
     // bytes4 constants for each method that can be called in a proposal
     // This will allow us to examine a proposed transaction and identify which method it calls,
     // so that we can then lookup in `req` how many approvals are required
-    bytes4 private constant SET_OWNER = bytes4("");
+    bytes4 private constant SET_OWNER_BYTES4 = bytes4("");
+    bytes4 private constant SET_ADMINS_BYTES4 = bytes4("");
 
     // Storage
+    uint nAdmins;
     mapping(address => bool) isAdmin;
     address owner;
     mapping(bytes4 => uint) reqs;
+    uint proposalCounter = 1;
+
 
     // modifiers
     modifier onlyOwner {
@@ -32,13 +36,15 @@ contract RecoveryWallet {
     constructor(
         address[] memory _admins,
         address _owner,
-//        uint _reqForSetAdmins,
+        uint _reqForSetAdmins,
         uint _reqForSetOwner
     ) public {
+        nAdmins = _admins.length;
         for (uint i = 0; i < _admins.length; i++) {
             isAdmin[_admins[i]] = true;
             owner = _owner;
-            reqs[SET_OWNER] = _reqForSetOwner;
+            reqs[SET_OWNER_BYTES4] = _reqForSetOwner;
+            reqs[SET_ADMINS_BYTES4] = _reqForSetAdmins;
         }
     }
 
@@ -47,5 +53,11 @@ contract RecoveryWallet {
             revert();
         }
         to.transfer(amount);
+    }
+
+    function () external payable {}
+
+    function getBalance() public view returns (uint res) {
+        return address(this).balance;
     }
 }
--- test/recovery-wallet.js
+++ test/recovery-wallet.js
@@ -1,6 +1,8 @@
 // import { RecoveryWallet as RecoveryWalletType } from "../build/typechain/RecoveryWallet";
+const BigNumber = require('bignumber.js')
 
 const RecoveryWallet = artifacts.require("./contracts/RecoveryWallet.sol");
+
 
 // Asserts the failure of a transaction.
 async function assertRevert(promise) {
@@ -13,20 +15,40 @@ async function assertRevert(promise) {
   }
 }
 
+async function assertBalance(address, amount) {
+  const balance = await web3.eth.getBalance(address)
+  assert(balance === amount, `Expected balance ${balance} to equal ${amount} for address ${address}`)
+}
+
 contract('RecoveryWallet', (accounts) => {
   // Truffle sends transactions from accounts[0] unless otherwise directed.
   let wallet
   let owner = accounts[1]
   let admins = accounts.slice(2, 5)
+  const otherAccount = web3.eth.accounts.create()
 
   beforeEach(async () => {
     // Deploy a new instance of BootnodeCoin to the network.
-    wallet = await RecoveryWallet.new(admins, owner, 2)
+    wallet = await RecoveryWallet.new(accounts.slice(2,5), accounts[1], 2, 2)
   })
 
   describe('#transfer', () => {
     it('should revert if the owner tries to transfer more than the wallet has', async () => {
-      assertRevert(wallet.transfer(accounts[2], 100))
+      assertRevert(wallet.transfer(accounts[2], 100, {from: accounts[1]}))
+    })
+
+    it('should transfer if the owner asks it it', async () => {
+      await wallet.send("100000")
+      await assertBalance(wallet.address, "100000")
+      await wallet.transfer(otherAccount.address, "40000", {from: accounts[1]})
+      await assertBalance(wallet.address, "60000")
+      await assertBalance(otherAccount.address, "40000")
+    })
+
+    it('should revert if someonelse else asks it it', async () => {
+      await wallet.send("100000")
+      await assertBalance(wallet.address, "100000")
+      assertRevert(wallet.transfer(otherAccount.address, "40000", {from: accounts[2]}))
     })
   })
 })
