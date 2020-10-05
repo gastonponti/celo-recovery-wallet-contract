4c8548bbb2678dc1c38468e4a35bac751483a10e
Or Neeman <or@clabs.co>
2020-10-05 09:43:12 -0600

    Set up react and webpack

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ import { newKit } from '@celo/contractkit';
 const kit = newKit('https://alfajores-forno.celo-testnet.org')
 
 // const RecoveryWalletContract = new kit.web3.eth.Contract(recoveryWalletJson['abi'], recoveryWalletJson['networks']['44787']['address']);
+
