d7ee587bb132c6648882b59757deb6f52b86f7ce
Or Neeman <or@clabs.co>
2020-10-05 14:17:15 -0600

    Proposing new owner works

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 const Web3 = require("web3")
-const web3 = new Web3("http://127.0.0.1:8545")
+const web3 = new Web3("ws://127.0.0.1:8545")
+import {Input} from "reactstrap"
 
 
 const walletJson = require('../build/contracts/RecoveryWallet.json');
@@ -36,19 +37,28 @@ async function deployToken() {
 
 const ready = setup()
 
-
 class App extends React.Component {
     constructor(args) {
         super(args)
         this.state = {
             admins: [],
             owner: "",
+            newOwnerProposals: [],
         }
     }
 
     async componentDidMount() {
         await ready;
         await this.reload();
+        wallet.events.SetOwnerProposed((err, ev) => this.handleSetOwnerProposed(err, ev));
+    }
+
+    handleSetOwnerProposed(err, ev) {
+        const data = ev.returnValues;
+        console.log(`Got new owner proposal: ${JSON.stringify(data)}`)
+        this.setState({
+            newOwnerProposals: [...this.state.newOwnerProposals, {id: data[0], address: data[1]}]
+        });
     }
 
     async reload() {
@@ -67,12 +77,56 @@ class App extends React.Component {
 
 
     render() {
-
         return (
             <div>
                 <h1>Recovery Wallet</h1>
-                <p>Owner: {accountNums[this.state.owner]}</p>
-                <p>Admins: {JSON.stringify(this.state.admins.map(x => accountNums[x]))}</p>
+                {this.renderWalletInfo()}
+                <NewOwnerProposer proposeSetOwner={(i) => this.proposeSetOwner(i)}/>
+                {this.renderNewOwnerProposals()}
+            </div>
+        )
+    }
+
+    renderWalletInfo() {
+        return (
+        <div>
+            <p>Owner: {accountNums[this.state.owner]}</p>
+            <p>Admins: {JSON.stringify(this.state.admins.map(x => accountNums[x]))}</p>
+        </div>)
+    }
+
+    renderNewOwnerProposals() {
+        const rows = this.state.newOwnerProposals.map(proposal => {
+            return <div key={proposal.id}>#{proposal.id}: proposes {proposal.address} (account {accountNums[proposal.address]})</div>
+        })
+        return <div>
+            <h3>New Owner Proposals</h3>
+            {rows}
+        </div>
+    }
+
+    proposeSetOwner(i) {
+        console.log(`Proposing account ${i}: ${accounts[i]}`)
+        wallet.methods.proposeSetOwner(accounts[i]).send({from: this.state.owner, gas: 500000});
+    }
+}
+
+class NewOwnerProposer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            i: 0
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Select a new owner to propose:
+                <Input type="select" onChange={(ev) => this.setState({i: ev.target.value})} value={this.state.i}>
+                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => <option value={i}>{i}</option>)}
+                </Input>
+                <button onClick={() => this.props.proposeSetOwner(this.state.i)}>Submit</button>
             </div>
         )
     }
